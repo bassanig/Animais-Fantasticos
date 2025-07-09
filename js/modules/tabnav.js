7a1c222d3d68780440a3a74e61a913bd131b0a23
Guilherme-Pc <guilherme.bassani07@gmail.com>
2025-07-09 19:04:16 -0300

    refactor(tabnav): muda de function para class para conseguir usar globalmente

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,22 +1,28 @@
-export default function initTabMenu() {
-    const tabmenu = document.querySelectorAll('[data-tab="menu"] li');
-    const tabcontent = document.querySelectorAll('[data-tab="content"] section');
+export default class initTabMenu {
+  constructor(menu, content) {
+    this.tabmenu = document.querySelectorAll(menu);
+    this.tabcontent = document.querySelectorAll(content);
+    this.activeClass = "active";
+  }
 
-    if (tabcontent.length && tabcontent.length) {
-    tabcontent[0].classList.add('active')
+  activeTab(index) {
+    const anime = this.tabcontent[index].dataset.anime;
+    this.tabcontent.forEach((item) => item.classList.remove("active"));
+    this.tabcontent[index].classList.add("active", anime);
+  }
 
-    function activeTab(index) {
-        const anime = tabcontent[index].dataset.anime;
-        tabcontent.forEach((item) => item.classList.remove('active'))
-        tabcontent[index].classList.add('active', anime);
+  addEventListeners() {
+    this.tabmenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        this.activeTab(index);
+      });
+    });
+  }
+
+  init() {
+    if (this.tabcontent.length && this.tabcontent.length) {
+      this.activeTab(0);
+      this.addEventListeners();
     }
-
-    tabmenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', () => {
-            activeTab(index)
-        })
-    })
-    }
+  }
 }
-
-initTabMenu()
